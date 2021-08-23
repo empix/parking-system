8357718a8b6fb88a1e6ace0fe1b4b83b8bbe453a
Ádrian <jotinhave@gmail.com>
2021-08-23 06:32:26 -0400

    Change view engine to handlebars

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 const path = require('path');
+const handlebars = require('express-handlebars');
 const express = require('express');
 const app = express();
 
 const router = require('./router');
 
-app.set('view engine', 'ejs');
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
