c5ba61ea86ba8ce67b62e7307c3b825858d00c2d
Ádrian <jotinhave@gmail.com>
2021-08-23 06:11:22 -0400

    Add middlewares to parse json and urlencoded

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const app = express();
 
 const router = require('./router');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(router);
 
 app.listen(process.env.PORT);
