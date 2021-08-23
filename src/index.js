ae777608873d91f551711a2e778d204e3d42c29c
Ádrian <jotinhave@gmail.com>
2021-08-23 06:20:21 -0400

    Add static folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const app = express();
 
 const router = require('./router');
 
+app.set('view engine', 'ejs');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/static', express.static(path.resolve(__dirname, 'public')));
 app.use(router);
 
 app.listen(process.env.PORT);
